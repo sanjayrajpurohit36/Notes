d2e794b67286d542dcc02762e5207d4b1321fb5f
Sanjay Rajpurohit <sanjay.rajpurohit@paytmmoney.com>
2022-01-11 23:36:32 +0530

    Updated State Management for sidebar component.

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,11 +1,14 @@
 import "./index.css";
 import { getTodaysDateAndTime } from "../../utils/timeHelper";
+import Tag from "./../Tag";
+
 const Card = (props) => {
-  const { name, desc, color } = props.data;
+  const { name, description, color } = props.data;
+
   return (
     <div className="card-wrapper" style={{ background: color }}>
       <div className="task-name">{`Task : ${name}`}</div>
-      <div className="task-desc">{`Description : ${desc}`}</div>
+      <div className="task-desc">{`Description : ${description}`}</div>
       <div className="timestamp">{`Created At : ${
         getTodaysDateAndTime()["date"] + ` ` + getTodaysDateAndTime()["time"]
       }`}</div>
