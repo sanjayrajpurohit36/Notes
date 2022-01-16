397bb0338d519fdb01b6b99cd3145be31a293bdd
Sanjay Rajpurohit <sanjay.rajpurohit@paytmmoney.com>
2022-01-16 22:33:50 +0530

    Added Fonts

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import "./assets/fonts/LuxuriousRoman-Regular.ttf";
+import "./assets/fonts/OpenSans-Italic-VariableFont_wdth,wght.ttf";
+import "./assets/fonts/OpenSans-VariableFont_wdth,wght.ttf";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
